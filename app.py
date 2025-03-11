1957545cc7d24708edaf7d32e0f3739969f5fbf4
hubimage <hubimage@126.com>
2025-03-11 17:07:30 +0800

    2025年3月11日更改 增加 神经网络模型推理
    2025年3月11日更改 增加 神经网络模型推理 ，但是无法支持多帧数据处理。

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from logg import log_warn, log_fatal, log_error
 from hub_city.app.utils import exception_handler
 from hub_city.app import web
 import time
+
 # video_format = 0
 
 '''
@@ -20,7 +21,8 @@ import time
 
 
 class BaseVar(object):
-    video_path = "trim_ipseek_copy.mp4"  # 视频路径
+    video_path = "trim_ipseek_copy.mp4"  # 视频路径 todo 2025年2月24日加 视频时长 20秒 分析用时  57S
+    # video_path = "de.mp4"   # todo 2025年2月24日加 视频时长 8分19秒  分析用时 668.2571303844452秒 11分
     save_image_size = (1500, 800)  # 保存图像尺寸
     image_threshold = 50000  # 精度阈值
     upload_step = 20  # 上传间隔
@@ -29,7 +31,7 @@ class BaseVar(object):
     real_area = 30  # 真实面积 单位: 平方千米
     longitude = 118.666098  # 精度
     latitude = 37.40491  # 维度
-    object_name = "soil"
+    object_name = ""
 
 
 def console(log_text):
@@ -125,14 +127,20 @@ class stream_inference(object):
                                               video_id=self.video_frame_id,
                                               name=config_var.object_name
                                               )
-
+        # print("dddddd")
         # self.video_frame_id += 1
-
+        # How it works How it works
         real_area = 30
         font_color = (255, 255, 255)
         result, result_area = image_draw(real_area, area, dst, self.dst_size, fill=font_color)
-        if self._post_:
-            self.post_filters(result_area, real_area, video_id, result)
+        # todo 2025年2月25日 更改  去吊self._post_, 加在 内部
+        #   任务1: 保存数据
+        #   任务2: 上传数据
+        #   version 1
+        # if self._post_:
+        #     self.post_filters(result_area, real_area, video_id, result)
+        # todo version 2
+        self.post_filters(result_area, real_area, video_id, result)
 
         if self._show_:
             show("image", result, 1)
@@ -157,26 +165,72 @@ class stream_inference(object):
             if not self.save_flag % config_var.upload_step:
                 if os.path.exists("result"):
                     save_image("./result/result1_%s.png" % video_id, result)
-                    fields = {
-                        "monitorvalue": str(result_area),
-                        "monitorname": "裸土面积",
-                        "monitorrate": str(result_area / real_area),
-                        "x": str(config_var.longitude),
-                        "y": str(config_var.latitude),
-                        "siteid": "null",
-                        "sitearea": str(config_var.real_area),
-                        "file": ("result1_%s.png" % video_id, open(
-                            "./result/result1_%s.png" % video_id,
-                            'rb'), "image/png")
-                    }
-                    upload(url=config_var.url, fields=fields)
+                    # todo 2025年2月25日加
+                    if self._post_:
+                        fields = {
+                            "monitorvalue": str(result_area),
+                            "monitorname": "裸土面积",
+                            "monitorrate": str(result_area / real_area),
+                            "x": str(config_var.longitude),
+                            "y": str(config_var.latitude),
+                            "siteid": "null",
+                            "sitearea": str(config_var.real_area),
+                            "file": ("result1_%s.png" % video_id, open(
+                                "./result/result1_%s.png" % video_id,
+                                'rb'), "image/png")
+                        }
+                        upload(url=config_var.url, fields=fields)
 
 
 def main():
-    rtsp_stream = stream_inference(_show_=True,)
+    rtsp_stream = stream_inference(_show_=False,)
     rtsp_stream.video_init()
 
 
 if __name__ == "__main__":
     config_var = BaseVar()
     main()
+# (I)
+# 《行业大模型构建经验分享》 (10:40-12:00)
+# 主讲人: 科大讯飞星火军团行业 AI 工程中心产品总监
+# 潘振兴
+# (三)《排水大模型应用系统开发经验分享》
+# (13:30-14:30)
+# 主讲人: 山东正元航空遥感技术有限公司遥感与智慧应用中心副经理宫龙
+# 1. 为啥选择？
+# 2. 交互方式：
+#   1. 语音识别输入。
+#   2. 点线面 画框交互方式。
+# 2. 知识库问答:
+#   本地知识库加载后 先搜索本地知识库 然后加载网络数据库(可能是第三方收集的)
+# 3. 界面设计：
+#   UI层上面加一个指令层,指令调用UI层。
+# 4. 管网查询: 交并查询
+#   小模型查询: 交 并 数据操作
+#   统计: 条件统计 自然语言输入 调出 各种弹出框
+#       隐患统计：
+#       管线统计：
+#       我们只是简单的做了工作，前期只是在做一些准备工作，实际开发中 我们只用了1个多月时间
+#       查询易涝点：
+#       查询防汛物资:
+#       查询防汛人员定位信息:
+#       合同处理：工单处理 有一个流程。
+#       报警信息: 语言播报。
+#   私密数据在本地 不在他们那边的。
+#   讯飞输入法: 输入法就是用的讯飞输入法。
+#   任务链：
+#   华为 做 图片
+#   讯飞 做 语音
+#   讯飞说 3000条以上QA
+#   大屏自动问数: 问表或图 自动生成 表或图
+#   收费方式: tokens 按照tokens收费的
+#       目前 所有的收费方式都是这样的。
+#       我们现在有些事情是商量的干的,收费并没有那么高。
+#       知识库这个事情 应该比较简单的了 deepseek有api 能直接接到我们的系统里面。
+#       知识库更新 一年更新一次 就了不得了
+#       外挂知识库 并不需要训练， 需要训练的话收费就比较高了,因为我们没有算力
+#       如果不及时更新数据库的话 别人查询最近报警信息 还是查询的1年前的呢?
+#
+
+
+
--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 from hub_city.app.image import hub_city, ModelParas
 from hub_city.app.utils import exception_handler
-
-
+import inspect
+# todo 2025年5月25日加
+from hub_city.app import image
 class Coe:
     def __init__(self):
         self.a1 = 0
@@ -62,21 +63,90 @@ def model_load(src,
                min_area,
                draw_color_fill,
                video_id,
-               name=''):
-    coe = Coe()
-    paras = compute_coefs(coe, 3)
+               name=None,
+               task=None):
+    # coe = Coe()
+    # paras = compute_coefs(coe, 3)
+    # todo 2025年2月25日 不用模型 直接四种模型数据写死在 里面
     # trans = 0
-    # hub_city.load_model("./model.obj")
-    hub_city.load_model("./hub_city-20250221.obj", name=name)
+    # if not name: # name为空 todo 2025年2月24日加
+    #     hub_city.load_model("./model.obj")
+    # else:
+    #     hub_city.load_model("./hub_city-20250221.obj", name=name)
+    # todo 2025年3月11日加 增加 task 参数 指定 神经网络的模型
+    hub_city.load_model(model_path="./green.obj", name='', task="mask_cnn")
+    # todo 2025年2月25日加 注释掉推理过程中的打印信息
+    image.__debug__console__ = False
     src, dst, area, video_id = hub_city.inference(src=src,
                                                   dst_size=dst_size,
                                                   min_area=min_area,
                                                   draw_color_fill=draw_color_fill,
                                                   video_id=video_id,
-                                                  params=paras, )
+                                                  params=0,
+                                                  mode=1,  # todo 2025年3月11日加 mode =1 深度学习推理 =0 传统模型推理
+                                                  )
     return src, dst, area, video_id
 
 
 @exception_handler
 def show(name, image, delay):
     hub_city.imshow(name, image, delay)
+
+# todo 2025年2月25日加
+# print(dir(ModelParas))
+# print(ModelParas.__dict__)
+# # 使用inspect模块的getmembers()函数查看MyClass类的所有属性, inspect属性可以解密到隐藏的属性参数
+# print(inspect.getmembers(ModelParas))
+# todo python二进制加密
+# 查看Python类的所有属性
+# 在Python中，查看一个类的所有属性可以通过多种方式实现。最常用的方法是使用内置的dir()函数，它可以列出对象的所有属性和方法。此外，还可以通过对象的__dict__属性或vars()函数来获取属性字典，或者使用inspect模块来进行更深入的反射操作。
+#
+# 使用dir()函数
+#
+# dir()函数可以在不带参数时列出当前范围内的变量、方法和定义的类型列表；带参数时，它会返回参数的属性、方法列表。如果参数包含方法__dir__()，该方法将被调用。如果参数不包含__dir__()，则会尽可能收集参数信息。例如：
+#
+# class MyClass:
+# def __init__(self):
+# self.attribute = "value"
+#
+# # 使用dir()函数查看MyClass类的所有属性
+# print(dir(MyClass))
+# 通过__dict__属性
+#
+# 每个Python对象都有一个__dict__属性，它包含了对象的属性字典。通过这个属性，可以直接访问对象的所有属性和它们的值。例如：
+#
+# class MyClass:
+# def __init__(self):
+# self.attribute = "value"
+#
+# # 实例化MyClass
+# my_object = MyClass()
+#
+# # 通过__dict__属性查看所有属性
+# print(my_object.__dict__)
+# 使用vars()函数
+#
+# vars()函数返回对象的__dict__属性，如果对象有__dict__属性的话。它与直接访问__dict__属性类似，但是vars()可以用于任何对象，不仅仅是实例。例如：
+#
+# class MyClass:
+# def __init__(self):
+# self.attribute = "value"
+#
+# # 实例化MyClass
+# my_object = MyClass()
+#
+# # 使用vars()函数查看所有属性
+# print(vars(my_object))
+# 使用inspect模块
+#
+# inspect模块提供了许多与Python对象交互的函数，其中getmembers()函数可以获取对象的所有成员，包括属性和方法。例如：
+#
+# import inspect
+#
+# class MyClass:
+# def __init__(self):
+# self.attribute = "value"
+#
+# # 使用inspect模块的getmembers()函数查看MyClass类的所有属性
+# print(inspect.getmembers(MyClass))
+# 通过这些方法，开发者可以方便地查看和访问Python类的所有属性，从而更好地理解和使用类的结构。
